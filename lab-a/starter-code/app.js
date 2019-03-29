71821385e8d132870248b6d9892de83ecac98ab4
williams osunkwo <wosunkwo@gmail.com>
2019-03-29 14:56:18 -0700

    solved question 1

--- lab-a/starter-code/app.js
+++ lab-a/starter-code/app.js
@@ -9,11 +9,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
+  var ansArr = [];
+  ansArr.push(a+b);
+  ansArr.push('The sum of '+ a + ' and '+ b + ' is '+ansArr[0]+ '.');
+  return ansArr;
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
